520fae221d1fff3772bc8a8b726d83a38c5c0a88
rk-laptop <ryukik1020@gmail.com>
2024-10-10 09:07:03 +0900

    文言修正

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 registered_users = os.environ["REGISTERED_USERS"].split(",")
 
 app = FastAPI()
-@app.get("/")
-def testPage():
-  return {"testss":"hello world"}
+
+
+# @app.get("/")
+# def testPage():
+#     return {"tests": "hello world"}
+
 
 # LineWebhookBodyモデル
 class LineWebhookBody(BaseModel):
     events: list
     destination: str
+
 
 # エンドポイント
 @app.post("/callback")
@@ -38,6 +42,7 @@ async def callback(request: Request):
         raise HTTPException(status_code=400)
     return "OK"
 
+
 # MessageEvent発生時（メッセージ受信時）
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
@@ -46,16 +51,16 @@ def handle_text_message(event):
     # 認証済みユーザーのみがアクセスできるようにする
     if user_id not in registered_users:
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text="認証に失敗しました。登録されたユーザーのみ使用できます。")
+            event.reply_token,
+            TextSendMessage(text="認証に失敗しました。登録されたユーザーのみ使用できます。"),
         )
         return
 
     # ユーザーからのメッセージを取得
     input_text = event.message.text
     # テキストに付与するメッセージ
-    # ex：add_message = "という質問に対してコーチング手法で回答してください"
-    add_message=""
-    prompt = f"User: {input_text}\n{add_message}\nChatGPT: "
+    add_message = "下記問い合わせに対し犬の真似をして可愛く答えてください"
+    prompt = f"User: {add_message}\n{input_text}\nChatGPT: "
 
     # botへのリクエストを生成
     try:
